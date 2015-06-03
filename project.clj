de29cca74f0dc201461e1d600e4a46b25636df42
Joshua Thayer <joshuathayer@riseup.net>
2015-06-03 15:13:17 -0700

    adds signing key

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.pff/java-libpst "0.8.1"]]
-  :main pst.core)
+  :main pst.core
+  :signing {:gpg-key "DC284680"})
